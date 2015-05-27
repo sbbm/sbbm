0a2849aa0bb39bd423c49b2d8bd8de6b2ccadd4f
Christopher Chambers <chris.chambers@peanutcode.com>
2015-05-27 15:07:59 -0500

    Reorganizing.
    The library consumed from SBBM code (targeting Minecraft) is now
    formally called libsbbm, and lives in that directory.
    
    Removes Cargo.lock from the root .gitignore and adds it to
    libsbbm/.gitignore.

--- libsbbm/build.rs
+++ libsbbm/build.rs
@@ -5,7 +5,7 @@ fn main() {
     let target = env::var("TARGET").unwrap();
     if target == "sbbm" {
         let proj_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-        let deps_path = Path::new(&proj_dir).join("deps");
+        let deps_path = Path::new(&proj_dir).parent().unwrap().join("deps");
         println!("cargo:rustc-link-search={}", deps_path.display());
     }
 }
